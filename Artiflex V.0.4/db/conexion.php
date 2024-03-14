38ce8f47b851950e126cad37e2a16607c038f7de
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-03-14 10:25:54 -0400

    Update conexion.php

--- Artiflex V.0.4/db/conexion.php
+++ Artiflex V.0.4/db/conexion.php
@@ -1,10 +1,8 @@
-
 <?php
-
 $host = "localhost";
-$usuario = "root";
-$clave = "";
-$base_de_datos = "artiflex_db";// base de datos nombre
+$usuario = "id21976653_root";
+$clave = "Luismanuel112289@";
+$base_de_datos = "id21976653_artiflex_db";// base de datos nombre
 
 $conexion = mysqli_connect($host, $usuario, $clave, $base_de_datos);
 
@@ -16,6 +14,4 @@ $conexion = mysqli_connect($host, $usuario, $clave, $base_de_datos);
 // {
 //     echo("lo sentimos no se pudo conectar a la base de datos");
 // }
-
-
 ?>
